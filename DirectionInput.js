442735a6ea157eddbc0098f8771cc3b116647204
Dezso Binder <binderdezso97@gmail.com>
2022-07-13 21:26:31 +0200

    Move hero, draw map, create character sprites

---
+++ DirectionInput.js
@@ -0,0 +1,35 @@
+class DirectionInput {
+  constructor() {
+    this.heldDirection = [];
+    this.map = {
+      ArrowUp: 'up',
+      ArrowDown: 'down',
+      ArrowLeft: 'left',
+      ArrowRight: 'right',
+      KeyW: 'up',
+      KeyA: 'left',
+      KeyS: 'down',
+      KeyD: 'right',
+    };
+  }
+
+  get direction() {
+    return this.heldDirection[0];
+  }
+
+  init() {
+    document.addEventListener('keydown', (e) => {
+      const dir = this.map[e.code];
+      if (dir && this.heldDirection.indexOf(dir) === -1) {
+        this.heldDirection.unshift(dir);
+      }
+    });
+    document.addEventListener('keyup', (e) => {
+      const dir = this.map[e.code];
+      const index = this.heldDirection.indexOf(dir);
+      if (index > -1) {
+        this.heldDirection.splice(index, 1);
+      }
+    });
+  }
+}
