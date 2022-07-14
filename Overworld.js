cfd49909c2f5fda4b75ee3eec77c048d5f10dd65
Dezso Binder <binderdezso97@gmail.com>
2022-07-14 12:35:18 +0200

    Setting cutscene mechanics, moving npcs

--- Overworld.js
+++ Overworld.js
@@ -21,9 +21,13 @@ class Overworld {
 
       this.map.drawLowerImage(this.ctx, cameraPerson);
 
-      Object.values(this.map.gameObjects).forEach((object) => {
-        object.sprite.draw(this.ctx, cameraPerson);
-      });
+      Object.values(this.map.gameObjects)
+        .sort((a, b) => {
+          return a.y - b.y;
+        })
+        .forEach((object) => {
+          object.sprite.draw(this.ctx, cameraPerson);
+        });
 
       this.map.drawUpperImage(this.ctx, cameraPerson);
 
@@ -40,5 +44,13 @@ class Overworld {
     this.directionInput = new DirectionInput();
     this.directionInput.init();
     this.startGameLoop();
+
+    this.map.startCutscene([
+      { who: 'hero', type: 'walk', direction: 'down' },
+      { who: 'hero', type: 'walk', direction: 'down' },
+      { who: 'npcA', type: 'walk', direction: 'left' },
+      { who: 'npcA', type: 'walk', direction: 'left' },
+      { who: 'npcA', type: 'stand', direction: 'up', time: 800 },
+    ]);
   }
 }
--- Person.js
+++ Person.js
@@ -17,7 +17,11 @@ class Person extends GameObject {
     if (this.movingProgressRemaining > 0) {
       this.updatePosition();
     } else {
-      if (this.isPlayerControlled && state.arrow) {
+      if (
+        !state.map.isCutscenePlaying &&
+        this.isPlayerControlled &&
+        state.arrow
+      ) {
         this.startBehavior(state, {
           type: 'walk',
           direction: state.arrow,
@@ -31,10 +35,24 @@ class Person extends GameObject {
     this.direction = behavior.direction;
     if (behavior.type === 'walk') {
       if (state.map.isSpaceTaken(this.x, this.y, this.direction)) {
+        behavior.retry &&
+          setTimeout(() => {
+            this.startBehavior(state, behavior);
+          }, 10);
+
         return;
       }
       state.map.moveWall(this.x, this.y, this.direction);
       this.movingProgressRemaining = 16;
+      this.updateSprite(state);
+    }
+
+    if (behavior.type === 'stand') {
+      setTimeout(() => {
+        utils.emitEvent('PersonStandComplete', {
+          whoId: this.id,
+        });
+      }, behavior.time);
     }
   }
 
@@ -42,6 +60,10 @@ class Person extends GameObject {
     const [property, change] = this.directionUpdate[this.direction];
     this[property] += change;
     this.movingProgressRemaining -= 1;
+
+    if (this.movingProgressRemaining === 0) {
+      utils.emitEvent('PersonWalkingComplete', { whoId: this.id });
+    }
   }
 
   updateSprite() {
