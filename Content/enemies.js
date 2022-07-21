70e0f97d87000a6b5edce66d4eb59beb40bec9f9
Dezso Binder <binderdezso97@gmail.com>
2022-07-21 17:35:16 +0200

    Stat changes and buffing enemy HP

--- Content/enemies.js
+++ Content/enemies.js
@@ -1,30 +1,30 @@
 window.Enemies = {
-  "erio": {
-    name: "Erio",
-    src: "/images/characters/people/erio.png",
+  erio: {
+    name: 'Erio',
+    src: '/images/characters/people/erio.png',
     pizzas: {
-      "a": {
-        pizzaId: "s001",
-        maxHp: 50,
+      a: {
+        pizzaId: 's001',
+        maxHp: 45,
         level: 1,
       },
-      "b": {
-        pizzaId: "s002",
-        maxHp: 50,
+      b: {
+        pizzaId: 's002',
+        maxHp: 45,
         level: 1,
       },
-    }
+    },
   },
-  "beth": {
-    name: "Beth",
-    src: "/images/characters/people/npc1.png",
+  beth: {
+    name: 'Beth',
+    src: '/images/characters/people/npc1.png',
     pizzas: {
-      "a": {
-        hp: 1,
-        pizzaId: "f001",
-        maxHp: 50,
+      a: {
+        hp: 30,
+        pizzaId: 'f001',
+        maxHp: 30,
         level: 1,
       },
-    }
-  }
-}
+    },
+  },
+};
