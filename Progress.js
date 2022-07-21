4a77d7536854bc0c6aa09472f19b4b06099256d4
Dezso Binder <binderdezso97@gmail.com>
2022-07-21 17:26:02 +0200

    Load game from saved file or create new game on titlescreen

--- Progress.js
+++ Progress.js
@@ -1,32 +1,35 @@
 class Progress {
   constructor() {
-    this.mapId = "Street";
+    this.mapId = 'DemoRoom';
     this.startingHeroX = 0;
     this.startingHeroY = 0;
-    this.startingHeroDirection = "down";
-    this.saveFileKey = "PizzaLegends_SaveFile1";
+    this.startingHeroDirection = 'down';
+    this.saveFileKey = 'PizzaLegends_SaveFile1';
   }
 
   save() {
-    window.localStorage.setItem(this.saveFileKey, JSON.stringify({
-      mapId: this.mapId,
-      startingHeroX: this.startingHeroX,
-      startingHeroY: this.startingHeroY,
-      startingHeroDirection: this.startingHeroDirection,
-      playerState: {
-        pizzas: playerState.pizzas,
-        lineup: playerState.lineup,
-        items: playerState.items,
-        storyFlags: playerState.storyFlags
-      }
-    }))
+    window.localStorage.setItem(
+      this.saveFileKey,
+      JSON.stringify({
+        mapId: this.mapId,
+        startingHeroX: this.startingHeroX,
+        startingHeroY: this.startingHeroY,
+        startingHeroDirection: this.startingHeroDirection,
+        playerState: {
+          pizzas: playerState.pizzas,
+          lineup: playerState.lineup,
+          items: playerState.items,
+          storyFlags: playerState.storyFlags,
+        },
+      }),
+    );
   }
 
   getSaveFile() {
     const file = window.localStorage.getItem(this.saveFileKey);
-    return file ? JSON.parse(file) : null
+    return file ? JSON.parse(file) : null;
   }
-  
+
   load() {
     const file = this.getSaveFile();
     if (file) {
@@ -34,10 +37,9 @@ class Progress {
       this.startingHeroX = file.startingHeroX;
       this.startingHeroY = file.startingHeroY;
       this.startingHeroDirection = file.startingHeroDirection;
-      Object.keys(file.playerState).forEach(key => {
+      Object.keys(file.playerState).forEach((key) => {
         playerState[key] = file.playerState[key];
-      })
+      });
     }
   }
-
 }
--- State/PlayerState.js
+++ State/PlayerState.js
@@ -1,46 +1,45 @@
 class PlayerState {
   constructor() {
     this.pizzas = {
-      "p1": {
-        pizzaId: "s001",
-        hp: 1,
-        maxHp: 50,
-        xp: 90,
-        maxXp: 100,
-        level: 1,
-        status: { type: "saucy" },
-      },
-      "p2": {
-        pizzaId: "v001",
+      p1: {
+        pizzaId: 's001',
         hp: 50,
         maxHp: 50,
-        xp: 75,
+        xp: 0,
         maxXp: 100,
         level: 1,
         status: null,
       },
-      "p3": {
-        pizzaId: "f001",
-        hp: 50,
-        maxHp: 50,
-        xp: 75,
-        maxXp: 100,
-        level: 1,
-        status: null,
-      }
-    }
-    this.lineup = ["p1"];
-    this.items = [
-      { actionId: "item_recoverHp", instanceId: "item1" },
-      { actionId: "item_recoverHp", instanceId: "item2" },
-      { actionId: "item_recoverHp", instanceId: "item3" },
-    ]
-    this.storyFlags = {
+      // "p2": {
+      //   pizzaId: "v001",
+      //   hp: 50,
+      //   maxHp: 50,
+      //   xp: 75,
+      //   maxXp: 100,
+      //   level: 1,
+      //   status: null,
+      // },
+      // "p3": {
+      //   pizzaId: "f001",
+      //   hp: 50,
+      //   maxHp: 50,
+      //   xp: 75,
+      //   maxXp: 100,
+      //   level: 1,
+      //   status: null,
+      // }
     };
+    this.lineup = ['p1'];
+    this.items = [
+      { actionId: 'item_recoverHp', instanceId: 'item1' },
+      { actionId: 'item_recoverHp', instanceId: 'item2' },
+      { actionId: 'item_recoverHp', instanceId: 'item3' },
+    ];
+    this.storyFlags = {};
   }
 
   addPizza(pizzaId) {
-    const newId = `p${Date.now()}`+Math.floor(Math.random() * 99999);
+    const newId = `p${Date.now()}` + Math.floor(Math.random() * 99999);
     this.pizzas[newId] = {
       pizzaId,
       hp: 50,
@@ -49,25 +48,24 @@ class PlayerState {
       maxXp: 100,
       level: 1,
       status: null,
-    }
+    };
     if (this.lineup.length < 3) {
-      this.lineup.push(newId)
+      this.lineup.push(newId);
     }
-    utils.emitEvent("LineupChanged");
-    console.log(this)
+    utils.emitEvent('LineupChanged');
+    console.log(this);
   }
 
   swapLineup(oldId, incomingId) {
     const oldIndex = this.lineup.indexOf(oldId);
     this.lineup[oldIndex] = incomingId;
-    utils.emitEvent("LineupChanged");
+    utils.emitEvent('LineupChanged');
   }
 
   moveToFront(futureFrontId) {
-    this.lineup = this.lineup.filter(id => id !== futureFrontId);
+    this.lineup = this.lineup.filter((id) => id !== futureFrontId);
     this.lineup.unshift(futureFrontId);
-    utils.emitEvent("LineupChanged");
+    utils.emitEvent('LineupChanged');
   }
-
 }
 window.playerState = new PlayerState();
